68fb952e58fa87c083ba09933d1843400a45450f
vudangitwork <91715618+vu0607@users.noreply.github.com>
2022-05-20 23:37:24 +0700

    Add files via upload

---
+++ Classify_Dog_Cat.py
@@ -0,0 +1,49 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPooling2D
+#train_dir = # your path to train dataset
+""" path = os.path.join(main_dir,train_dir)
+
+for p in os.listdir(path):
+    category = p.split(".")[0]
+    img_array = cv2.imread(os.path.join(path,p),cv2.IMREAD_GRAYSCALE)
+    new_img_array = cv2.resize(img_array, dsize=(80, 80))
+    plt.imshow(new_img_array,cmap="gray")
+    break
+X = []
+y = []
+convert = lambda category : int(category == 'dog')
+def create_test_data(path):
+    for p in os.listdir(path):
+        category = p.split(".")[0]
+        category = convert(category)
+        img_array = cv2.imread(os.path.join(path,p),cv2.IMREAD_GRAYSCALE)
+        new_img_array = cv2.resize(img_array, dsize=(80, 80))
+        X.append(new_img_array)
+        y.append(category)
+create_test_data(path) """
+X = np.array(X).reshape(-1, 80,80,1)
+y = np.array(y)
+#Normalize data
+X = X/255.0
+model = Sequential()
+# Adds a densely-connected layer with 64 units to the model:
+model.add(Conv2D(64,(3,3), activation = 'relu', input_shape = X.shape[1:]))
+model.add(MaxPooling2D(pool_size = (2,2)))
+# Add another:
+model.add(Conv2D(64,(3,3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+# Add a softmax layer with 10 output units:
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer="adam",
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.fit(X, y, epochs=10, batch_size=32, validation_split=0.2)
